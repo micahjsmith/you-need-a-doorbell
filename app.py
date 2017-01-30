29a0de79e45c75a1f09f7d1f19c695998f46b00f
Micah Smith <micahjsmith@gmail.com>
2017-01-29 23:21:40 -0500

    Move options to a conf file, refactoring

--- app.py
+++ app.py
@@ -1,49 +1,120 @@
 from flask import Flask, request, redirect
-
+import twilio.twiml
 import subprocess
 import time
-
-import twilio.twiml
+import configparser
+import tempfile
+import os
+from shutil import which
 
 app = Flask(__name__)
 
-log_filename='/Users/micahsmith/partyapp/arrivals.log'
-micah_number = "+15555551234"
-already_arrived = []
+sms_path = "/party_arrival"
+DEFAULT_CONFIG_FILE_LOCATION = os.path.join(
+        os.path.expanduser("~"),".partyapp.conf")
+
+# Settings
+c = None
+lists = {}
+lists["hostlist"]  = {}
+lists["whitelist"] = {}
+lists["blacklist"] = {}
+lists["arrivals"]  = {}
+
+def load_config():
+    config = configparser.ConfigParser()
+    config.read_file(open("defaults.conf"))
+    config.read([DEFAULT_CONFIG_FILE_LOCATION])
+
+    # copy hostlist, whitelist, and blacklist
+    for l in ["hostlist", "whitelist", "blacklist"]:
+        lists[l] = dict(config[l])
+
+    return dict(config["app"])
+
+def validate_config(c):
+    # Ensure that arrival_message has at most one '{}'
+    if c["arrival_message"].count("{}") > 1:
+        raise ValueError("arrival_message must contain at most one '{}'")
+
+    # Ensure that tts prog ram exists
+    if which(c["tts_command"]) is None:
+        raise EnvironmentError("unable to verify that tts command {} is callable".format(c["tts_command"]))
+
+    # Ensure log file directory exists
+    if c["log_file"] == "_":
+        d = tempfile.mkdtemp()
+        f = os.path.join(d, "party_arrivals.log")
+        c["log_file"] = f
+    else:
+        d = os.path.dirname(c["log_file"])
+        if not os.path.exists(d):
+            os.makedirs(d)
+
+    print('Logging to {}'.format(c["log_file"]))
+
+    return True
+>>>>>>> Move options to a conf file, refactoring
 
 @app.route("/", methods=['GET'])
 def hello():
-    subprocess.call(["say", "Hello World"])
     return "Hello World"
 
-def announce(person):
-    message = 'DING DONG. Guest {} has arrived to the party.'.format(person)
-    subprocess.call(["say", message])
-
-@app.route("/party_arrival", methods=['POST'])
-def party_arrival():
-    number = request.form['From']
-    message_body = request.form['Body']
-
-    if number == micah_number or number not in already_arrived:
-        result = "announced"
-        already_arrived.append(number)
-        announce(message_body)
-        resp = twilio.twiml.Response()
-        resp.message('Someone is coming to open the door. (Make sure you are at the Cross St entrance.)')
-        ret = str(resp)
-    else:
-        result = "repeat"
-        resp = twilio.twiml.Response()
-        resp.message('You have already announced your arrival.')
-        ret = str(resp)
-
-    with open(log_filename, "a") as f:
+def log(sms_number, sms_name, result):
+    with open(c["log_file"], "a") as f:
         tm = time.strftime("%Y-%m-%d %H:%M")
-        log_message = '[{}] {}: {} => {}\n'.format(tm, number, message_body, result)
+        log_message = '[{}] {}: {} => {}\n'.format(
+                tm, sms_number, sms_name, result)
         f.write(log_message)
+
+def announce(person):
+    message = c["doorbell_text"] + " " + c["arrival_message"].format(person)
+    subprocess.call([c["tts_command"], c["tts_command_options"], message])
+
+def reply(message):
+    resp = twilio.twiml.Response()
+    resp.message(message)
+    return str(resp)
+
+@app.route(sms_path, methods=['POST'])
+def party_arrival():
+    sms_number = request.form['From']
+    sms_name = request.form['Body']
+
+    is_host = sms_number in lists["hostlist"].values()
+    is_approved = (sms_number in lists["whitelist"].values() or
+        (not any(lists["whitelist"]) and sms_number not in lists["blacklist"].values()))
+    has_arrived = sms_number in lists["arrivals"].values()
+
+    if is_host or (is_approved and not has_arrived):
+        result = "announced"
+
+        # Add to list of arrivals
+        lists["arrivals"][sms_name] = sms_number
+
+        # Announce
+        announce(sms_name)
+
+        # Text back
+        ret = reply(c["arrival_response"])
+    elif is_approved and has_arrived:
+        result = "repeat"
+
+        # Text back
+        ret = reply(c["repeat_response"])
+    elif not is_approved:
+        result = "rejected"
+        ret = ""
+    else:
+        result = "other"
+        ret = ""
+
+    # Log the result
+    log(sms_number, sms_name, result)
 
     return ret
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    c = load_config()
+    validate_config(c)
+    app.run(host=c["host"], port=c["port"])
