6e4c1b818ddcca0578455fbdbfa96cf3e9a7fdee
Micah Smith <micahjsmith@gmail.com>
2018-01-29 15:46:40 -0500

    Begin developing table of gatherings

---
+++ ios-app/YouNeedADoorbell/YouNeedADoorbell/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  YouNeedADoorbell
+//
+//  Created by Micah Smith on 1/29/18.
+//  Copyright © 2018 Micah Smith. All rights reserved.
+//
+
+import Foundation
+
+class Gathering {
+    var title: String? = nil
+}
