c69639d2ebe1df9928d85fde7372c48a65950ccb
Micah Smith <micahjsmith@gmail.com>
2018-02-05 15:37:46 -0500

    Figure out how to access request body

--- ios-app/cloudfunctions/functions/index.js
+++ ios-app/cloudfunctions/functions/index.js
@@ -1,36 +1,29 @@
 'use strict';
 
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
-
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
-
 // Twilio response
 // adapted from https://cloud.google.com/community/tutorials/cloud-functions-twilio-sms
+//
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 const twilio = require('twilio');
 const config = require('./config.json');
 const MessagingResponse = twilio.twiml.MessagingResponse;
+admin.initializeApp(functions.config().firebase);
 
-exports.replyToSms = functions.https.onRequest( (req, res) => {
+exports.replyToSms = functions.https.onRequest((request, response) => {
   let isValid = true;
   
   // Only validate that requests came from Twilio when the function has been
   // deployed to production.
   if (process.env.NODE_ENV === 'production') {
-    isValid = twilio.validateExpressRequest(req, config.TWILIO_AUTH_TOKEN, {
+    isValid = twilio.validateExpressRequest(request, config.TWILIO_AUTH_TOKEN, {
       url: `https://${config.GCLOUD_REGION}-${config.GFIREBASE_PROJECTID}.cloudfunctions.net/replyToSms`
     });
   }
 
   // Halt early if the request was not sent from Twilio
   if (!isValid) {
-    res
+    response
       .type('text/plain')
       .status(403)
       .send('Twilio Request Validation Failed.')
@@ -39,14 +32,15 @@ exports.replyToSms = functions.https.onRequest( (req, res) => {
   }
 
   // Prepare a response to the SMS message
-  const response = new MessagingResponse();
+  const messagingResponse = new MessagingResponse();
 
   // Add text to the response
-  response.message('Hello from Google Cloud Functions!');
+  messagingResponse.message('Hello from Google Cloud Functions! You sent a message to ' + request.body['To'] + ' from ' + request.body['From']);
 
   // Send the response
-  res
+  response
     .status(200)
     .type('text/xml')
-    .end(response.toString());
+    .end(messagingResponse.toString());
+  return;
 });
